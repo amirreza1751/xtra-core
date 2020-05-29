c1590d45fddfe7c04a273c00550d532953e1ae04
Alireza dHb <Alireza.dhb@gmail.com>
2020-05-29 11:39:02 +0430

    initilize

---
+++ src/test/java/com/xtra/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtra.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
