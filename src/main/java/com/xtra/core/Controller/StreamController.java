a5ff2d7ac3709f9990923344e98875b7cc5e7dfd
amirak <dashti.amir2752@gmail.com>
2020-06-09 01:11:36 +0430

    return m3u8 playlist

--- src/main/java/com/xtra/core/Controller/StreamController.java
+++ src/main/java/com/xtra/core/Controller/StreamController.java
@@ -1,15 +1,40 @@
 package com.xtra.core.Controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 public class StreamController {
-    @GetMapping("/auth")
-    ResponseEntity<String> authenticateUser(){
-        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+
+    @RequestMapping(
+            value = "/auth"
+    )
+    public @ResponseBody Object authenticateUser (/*0 @RequestParam("user_unique_id") String user_unique_id */ @RequestParam Map<String,String> allRequestParams) throws IOException {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        File file = ResourceUtils.getFile("/home/amirak/xtreamcodes/iptv_xtream_codes/streams/" + allRequestParams.get("stream_unique_id") + "_." + allRequestParams.get("extension"));
+        String playlist = new String(Files.readAllBytes(file.toPath()));
+
+
+        Pattern pattern = Pattern.compile("(.*)\\.ts");
+        Matcher match = pattern.matcher(playlist);
+
+        System.out.println("Found value: " + match.group(0) );
+
+        return ResponseEntity.ok()
+                .headers(responseHeaders).contentType(MediaType.valueOf("application/x-mpegurl"))
+                .headers(responseHeaders).contentLength(Long.parseLong(String.valueOf(playlist.length())))
+                .headers(responseHeaders).cacheControl(CacheControl.noCache())
+                .headers(responseHeaders).cacheControl(CacheControl.noStore())
+                .header("Content-Disposition", "inline; filename=" + "\"" +file.getName() + "\"")
+                .body(playlist);
     }
 
 }
