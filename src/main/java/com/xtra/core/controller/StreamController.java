97de2d2e54c6cc10403a7472f02e7d78eb14c767
Alireza dHb <Alireza.dhb@gmail.com>
2020-06-09 15:41:23 +0430

    remove bullshit

--- src/main/java/com/xtra/core/controller/StreamController.java
+++ src/main/java/com/xtra/core/controller/StreamController.java
@@ -21,16 +21,13 @@ public class StreamController {
         HttpHeaders responseHeaders = new HttpHeaders();
         String userHome = System.getProperty("user.home");
         File file = ResourceUtils.getFile(userHome + "/streams/" + stream_id + "_." + extension);
-        File file = ResourceUtils.getFile("/home/amirak/xtreamcodes/iptv_xtream_codes/streams/" + allRequestParams.get("stream_unique_id") + "_." + allRequestParams.get("extension"));
-
         String playlist = new String(Files.readAllBytes(file.toPath()));
-
 
         Pattern pattern = Pattern.compile("(.*)\\.ts");
         Matcher match = pattern.matcher(playlist);
 
         while (match.find()) {
-            playlist = playlist.replace(match.group(0), "/home/amirak/xtreamcodes/iptv_xtream_codes/streams/" + match.group(0));
+            playlist = playlist.replace(match.group(0), "" + match.group(0));
         }
         System.out.println(playlist);
         return ResponseEntity.ok()
