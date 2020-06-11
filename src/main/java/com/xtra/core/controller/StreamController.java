ff51495e906a379ba75059e6381e3cb717b90a69
Alireza dHb <Alireza.dhb@gmail.com>
2020-06-11 23:37:11 +0430

    add segment endpoint

--- src/main/java/com/xtra/core/controller/StreamController.java
+++ src/main/java/com/xtra/core/controller/StreamController.java
@@ -1,9 +1,16 @@
 package com.xtra.core.controller;
 
-import org.springframework.http.*;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.ResourceUtils;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,23 +20,21 @@ import java.util.regex.Pattern;
 @RestController
 public class StreamController {
 
-    @RequestMapping(
-            value = "/auth"
-    )
+    @GetMapping("/streams")
     public @ResponseBody
-    ResponseEntity<String> authenticateUser(@RequestParam String user_id, @RequestParam String stream_id, @RequestParam String extension) throws IOException {
+    ResponseEntity<String> GetPlaylist(@RequestParam String user_id, @RequestParam String stream_id
+            , @RequestParam String extension) throws IOException {
         HttpHeaders responseHeaders = new HttpHeaders();
-        String userHome = System.getProperty("user.home");
-        File file = ResourceUtils.getFile(userHome + "/streams/" + stream_id + "_." + extension);
+        File file = ResourceUtils.getFile(System.getProperty("user.home") + "/streams/" + stream_id + "_." + extension);
         String playlist = new String(Files.readAllBytes(file.toPath()));
 
         Pattern pattern = Pattern.compile("(.*)\\.ts");
         Matcher match = pattern.matcher(playlist);
 
         while (match.find()) {
-            playlist = playlist.replace(match.group(0), "" + match.group(0));
+            playlist = playlist.replace(match.group(0), String.format("/hls/%s/", user_id) + match.group(0));
         }
-        System.out.println(playlist);
+
         return ResponseEntity.ok()
                 .headers(responseHeaders).contentType(MediaType.valueOf("application/x-mpegurl"))
                 .headers(responseHeaders).contentLength(Long.parseLong(String.valueOf(playlist.length())))
@@ -37,6 +42,16 @@ public class StreamController {
                 .headers(responseHeaders).cacheControl(CacheControl.noStore())
                 .header("Content-Disposition", "inline; filename=" + "\"" + file.getName() + "\"")
                 .body(playlist);
+    }
+
+    @GetMapping("segment")
+    public @ResponseBody
+    ResponseEntity<byte[]> PlaySegment(@RequestParam String user_id, @RequestParam String stream_id
+            , @RequestParam String extension, @RequestParam String segment_id) throws IOException {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        return ResponseEntity.ok()
+                .headers(responseHeaders).contentType(MediaType.valueOf("video/mp2t"))
+                .body(IOUtils.toByteArray(FileUtils.openInputStream(new File(System.getProperty("user.home") + "/streams/" + stream_id + "_" + segment_id + "." + extension))));
     }
 
 }
