e9359b258c1b5ab7615213c8a3ecd77586043623
amirak <dashti.amir2752@gmail.com>
2020-06-13 23:16:49 +0430

    test

--- src/main/java/com/xtra/core/controller/VideoStreamController.java
+++ src/main/java/com/xtra/core/controller/VideoStreamController.java
@@ -2,7 +2,6 @@ package com.xtra.core.controller;
 
 import com.xtra.core.service.VideoStreamService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -17,7 +16,7 @@ public class VideoStreamController {
     }
 
     @GetMapping("/stream/{fileType}/{fileName}")
-    public Mono<ResponseEntity<byte[]>> streamVideo(ServerHttpResponse serverHttpResponse, @RequestHeader(value = "Range", required = false) String httpRangeList,
+    public Mono<ResponseEntity<byte[]>> streamVideo( @RequestHeader(value = "Range", required = false) String httpRangeList,
                                                     @PathVariable("fileType") String fileType,
                                                     @PathVariable("fileName") String fileName) {
         return Mono.just(videoStreamService.prepareContent(fileName, fileType, httpRangeList));
