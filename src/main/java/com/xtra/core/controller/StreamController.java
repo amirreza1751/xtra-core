1e4333005bc850512ed1fcfe21e3295b1c63a081
Alireza dHb <Alireza.dhb@gmail.com>
2020-06-13 22:54:42 +0430

    complete streaming from url

--- src/main/java/com/xtra/core/controller/StreamController.java
+++ src/main/java/com/xtra/core/controller/StreamController.java
@@ -1,19 +1,20 @@
 package com.xtra.core.controller;
 
+import com.xtra.core.model.Line;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.springframework.http.CacheControl;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.time.LocalDateTime;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,6 +26,12 @@ public class StreamController {
     ResponseEntity<String> GetPlaylist(@RequestParam String user_id, @RequestParam String stream_id
             , @RequestParam String extension) throws IOException {
         HttpHeaders responseHeaders = new HttpHeaders();
+        ResponseEntity<String> response;
+        RestTemplate restTemplate = new RestTemplate();
+//        Line line = restTemplate.getForObject("http://localhost:8082/api/users/" + user_id, Line.class);
+//        if(line.getExpireDate().compareTo(LocalDateTime.now()) < 0){
+//            response = ResponseEntity.status(403).body("Line Expired");
+//        }
         File file = ResourceUtils.getFile(System.getProperty("user.home") + "/streams/" + stream_id + "_." + extension);
         String playlist = new String(Files.readAllBytes(file.toPath()));
 
@@ -32,7 +39,8 @@ public class StreamController {
         Matcher match = pattern.matcher(playlist);
 
         while (match.find()) {
-            playlist = playlist.replace(match.group(0), String.format("/hls/%s/", user_id) + match.group(0));
+            String link = match.group(0);
+            playlist = playlist.replace(match.group(0), String.format("http://localhost:8081/hls/%s/%s/%s", user_id, stream_id, link.split("_")[1]));
         }
 
         return ResponseEntity.ok()
@@ -47,11 +55,11 @@ public class StreamController {
     @GetMapping("segment")
     public @ResponseBody
     ResponseEntity<byte[]> PlaySegment(@RequestParam String user_id, @RequestParam String stream_id
-            , @RequestParam String extension, @RequestParam String segment_id) throws IOException {
+            , @RequestParam String extension, @RequestParam String segment) throws IOException {
         HttpHeaders responseHeaders = new HttpHeaders();
         return ResponseEntity.ok()
                 .headers(responseHeaders).contentType(MediaType.valueOf("video/mp2t"))
-                .body(IOUtils.toByteArray(FileUtils.openInputStream(new File(System.getProperty("user.home") + "/streams/" + stream_id + "_" + segment_id + "." + extension))));
+                .body(IOUtils.toByteArray(FileUtils.openInputStream(new File(System.getProperty("user.home") + "/streams/" + stream_id + "_" + segment + "." + extension))));
     }
 
 }
