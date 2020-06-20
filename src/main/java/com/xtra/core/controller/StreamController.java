581b559a08db625bec5aafa804cb42f65f0d000e
Alireza dHb <Alireza.dhb@gmail.com>
2020-06-20 21:48:07 +0430

    add stream authentication

--- src/main/java/com/xtra/core/controller/StreamController.java
+++ src/main/java/com/xtra/core/controller/StreamController.java
@@ -28,28 +28,35 @@ public class StreamController {
         HttpHeaders responseHeaders = new HttpHeaders();
         ResponseEntity<String> response;
         RestTemplate restTemplate = new RestTemplate();
-//        Line line = restTemplate.getForObject("http://localhost:8082/api/users/" + user_id, Line.class);
-//        if(line.getExpireDate().compareTo(LocalDateTime.now()) < 0){
-//            response = ResponseEntity.status(403).body("Line Expired");
-//        }
-        File file = ResourceUtils.getFile(System.getProperty("user.home") + "/streams/" + stream_id + "_." + extension);
-        String playlist = new String(Files.readAllBytes(file.toPath()));
+        Line line = restTemplate.getForObject("http://localhost:8082/api/users/" + user_id, Line.class);
+        if(line.getExpireDate().compareTo(LocalDateTime.now()) < 0){
+            response = ResponseEntity.status(403).body("Line Expired");
+        }else if(line.isBlocked()){
+            response = ResponseEntity.status(403).body("Line is Blocked");
+        }else if(line.isAdminBlocked()){
+            response = ResponseEntity.status(403).body("Line is Blocked By Admin");
+        }
+        else {
+            File file = ResourceUtils.getFile(System.getProperty("user.home") + "/streams/" + stream_id + "_." + extension);
+            String playlist = new String(Files.readAllBytes(file.toPath()));
 
-        Pattern pattern = Pattern.compile("(.*)\\.ts");
-        Matcher match = pattern.matcher(playlist);
+            Pattern pattern = Pattern.compile("(.*)\\.ts");
+            Matcher match = pattern.matcher(playlist);
 
-        while (match.find()) {
-            String link = match.group(0);
-            playlist = playlist.replace(match.group(0), String.format("http://localhost:8081/hls/%s/%s/%s", user_id, stream_id, link.split("_")[1]));
+            while (match.find()) {
+                String link = match.group(0);
+                playlist = playlist.replace(match.group(0), String.format("http://localhost:8081/hls/%s/%s/%s", user_id, stream_id, link.split("_")[1]));
+            }
+            response = ResponseEntity.ok()
+                    .headers(responseHeaders).contentType(MediaType.valueOf("application/x-mpegurl"))
+                    .headers(responseHeaders).contentLength(Long.parseLong(String.valueOf(playlist.length())))
+                    .headers(responseHeaders).cacheControl(CacheControl.noCache())
+                    .headers(responseHeaders).cacheControl(CacheControl.noStore())
+                    .header("Content-Disposition", "inline; filename=" + "\"" + file.getName() + "\"")
+                    .body(playlist);
         }
 
-        return ResponseEntity.ok()
-                .headers(responseHeaders).contentType(MediaType.valueOf("application/x-mpegurl"))
-                .headers(responseHeaders).contentLength(Long.parseLong(String.valueOf(playlist.length())))
-                .headers(responseHeaders).cacheControl(CacheControl.noCache())
-                .headers(responseHeaders).cacheControl(CacheControl.noStore())
-                .header("Content-Disposition", "inline; filename=" + "\"" + file.getName() + "\"")
-                .body(playlist);
+        return response;
     }
 
     @GetMapping("segment")
