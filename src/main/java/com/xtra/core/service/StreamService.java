954ab3628d26258835cd9fea269a90f0c4e2d59a
amirak <dashti.amir2752@gmail.com>
2020-06-29 14:05:26 +0430

    modifying ffmpeg command & creating live-streams directory

--- src/main/java/com/xtra/core/service/StreamService.java
+++ src/main/java/com/xtra/core/service/StreamService.java
@@ -3,16 +3,29 @@ package com.xtra.core.service;
 import com.xtra.core.model.Stream;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
 
 @Service
 public class StreamService {
     public long StartStream(Stream stream){
+
+        File liveStreamsDirectory = new File(
+                System.getProperty("user.home") + File.separator +
+                        "xtra-project" + File.separator +
+                        "xtra-core" + File.separator +
+                        "live-streams"
+        );
+        if (!liveStreamsDirectory.exists()){
+            liveStreamsDirectory.mkdirs();
+        }
+
         String[] args = new String[] {
                 "ffmpeg",
                 "-re",
                 "-i",
-                "http://goiptv.co:8080/live:cross2hosting/cIfILzx9ji/3994",
+//                "http://goiptv.co:8080/live:cross2hosting/cIfILzx9ji/3994",
+                stream.getCurrentInput().getUrl(),
                 "-vcodec",
                 "copy",
                 "-loop",
@@ -26,7 +39,7 @@ public class StreamService {
                 "-strict",
                 "-2",
                 "-f",
-                "segment",
+                "hls",
                 "-segment_format",
                 "mpegts",
                 "-segment_time",
@@ -40,13 +53,16 @@ public class StreamService {
                 "-hls_flags",
                 "delete_segments",
                 "-segment_list",
-                "/home/amirak/xtreamcodes/iptv_xtream_codes/streams/57852_.m3u8",
-                "/home/amirak/xtreamcodes/iptv_xtream_codes/streams/57852_%d.ts"
+                liveStreamsDirectory.getAbsolutePath() + "/" + stream.getId() + "_%d.ts",
+                liveStreamsDirectory.getAbsolutePath()  + "/" + stream.getId()+ "_.m3u8"
         };
-//        String args = "ffmpeg -re -i http://goiptv.co:8080/live:cross2hosting/cIfILzx9ji/3994 -vcodec copy -loop -1 -c:a aac -b:a
-//        160k -ar 44100 -strict -2 -f segment -segment_format mpegts -segment_time 10 -segment_list_size 6 -segment_format_options
-//        mpegts_flags=+initial_discontinuity:mpegts_copyts=1 -segment_list_type m3u8 -hls_flags delete_segments -segment_list
-//        /home/amirak/xtreamcodes/iptv_xtream_codes/streams/57852_.m3u8 /home/amirak/xtreamcodes/iptv_xtream_codes/streams/57852_%d.ts ";
+//        ffmpeg -re -i http://goiptv.co:8080/live:cross2hosting/cIfILzx9ji/3994
+//        -vcodec copy -loop -1 -c:a aac -b:a 160k -ar 44100 -strict -2
+//        -f hls -segment_format mpegts -segment_time 10 -segment_list_size 6
+//        -segment_format_options mpegts_flags=+initial_discontinuity:mpegts_copyts=1
+//        -segment_list_type m3u8 -hls_flags delete_segments -segment_list
+//        /home/amirak/xtreamcodes/iptv_xtream_codes/streams/57852_%d.ts
+//        /home/amirak/xtreamcodes/iptv_xtream_codes/streams/57852_.m3u8 ";
         Process proc;
         try {
             proc = new ProcessBuilder(args).start();
