72ef96de097614ad17f6c2a42435e9477a89b190
amirak <dashti.amir2752@gmail.com>
2020-07-22 08:05:57 +0430

    remove Stream Repository

--- src/main/java/com/xtra/core/repository/StreamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.xtra.core.repository;
-
-import com.xtra.core.model.Stream;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StreamRepository extends JpaRepository<Stream, Long> {
-}
--- src/main/java/com/xtra/core/schedule/CoreTaskScheduler.java
+++ src/main/java/com/xtra/core/schedule/CoreTaskScheduler.java
@@ -1,9 +1,7 @@
 package com.xtra.core.schedule;
 
-import com.xtra.core.model.Stream;
 import com.xtra.core.model.StreamInfo;
 import com.xtra.core.repository.ProcessRepository;
-import com.xtra.core.repository.StreamRepository;
 import com.xtra.core.service.ProcessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,19 +10,16 @@ import com.xtra.core.model.Process;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Component
 public class CoreTaskScheduler {
     private final ProcessRepository processRepository;
     private final ProcessService processService;
-    private final StreamRepository streamRepository;
 
     @Autowired
-    public CoreTaskScheduler(ProcessRepository processRepository, ProcessService processService, StreamRepository streamRepository) {
+    public CoreTaskScheduler(ProcessRepository processRepository, ProcessService processService) {
         this.processRepository = processRepository;
         this.processService = processService;
-        this.streamRepository = streamRepository;
     }
 
     @Scheduled(fixedDelay = 5000)
@@ -32,7 +27,6 @@ public class CoreTaskScheduler {
         List<StreamInfo> streamInfoList = new ArrayList<>();
         for (Process process : processRepository.findAll()) {
             var uptime = processService.getProcessEtime(process.getPid());
-            Optional<Stream> stream = streamRepository.findById(process.getStreamId());
             streamInfoList.add(new StreamInfo(process.getStreamId(), uptime, "", "", "", "", "", "", ""));
         }
         //call api here;
