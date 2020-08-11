bf81f4e8ef3175e4a4d4e7ebe418ddfb34646baf
amirak <dashti.amir2752@gmail.com>
2020-08-11 20:54:39 +0430

    add FileSystemController

---
+++ src/main/java/com/xtra/core/controller/FileSystemController.java
@@ -0,0 +1,20 @@
+package com.xtra.core.controller;
+import com.xtra.core.repository.LineActivityRepository;
+import com.xtra.core.repository.ProgressInfoRepository;
+import com.xtra.core.service.LineService;
+import com.xtra.core.service.StreamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class FileSystemController {
+
+
+    @Autowired
+    public FileSystemController() {}
+
+
+
+}
