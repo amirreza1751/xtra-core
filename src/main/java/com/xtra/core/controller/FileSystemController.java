21edfc6c2d630683ddd6fd2b7857f519bf65625e
Alireza Dhb <alireza.dhb@gmail.com>
2020-08-13 18:40:33 +0430

    small fix

--- src/main/java/com/xtra/core/controller/FileSystemController.java
+++ src/main/java/com/xtra/core/controller/FileSystemController.java
@@ -2,6 +2,7 @@ package com.xtra.core.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping("/file")
 public class FileSystemController {
 
 
@@ -18,22 +20,24 @@ public class FileSystemController {
     public FileSystemController() {
     }
 
-    @GetMapping("list")
+    @GetMapping("/list_files")
     public List<com.xtra.core.model.File> list(@RequestParam String path) {
         File directoryPath = new File(path);
         if (!directoryPath.exists() || !path.startsWith(System.getProperty("user.home"))) {
-            return new ArrayList<com.xtra.core.model.File>();
+            return new ArrayList<>();
         }
         File[] filesList = directoryPath.listFiles();
         ArrayList<com.xtra.core.model.File> result = new ArrayList<>();
-        for (File file : filesList) {
-            result.add(
-                    new com.xtra.core.model.File(
-                            file.getName(),
-                            file.getAbsolutePath(),
-                            (file.isDirectory()) ? file.getTotalSpace() : file.length(),
-                            file.isDirectory())
-            );
+        if (filesList != null) {
+            for (File file : filesList) {
+                result.add(
+                        new com.xtra.core.model.File(
+                                file.getName(),
+                                file.getAbsolutePath(),
+                                (file.isDirectory()) ? file.getTotalSpace() : file.length(),
+                                file.isDirectory())
+                );
+            }
         }
 
         return result;
