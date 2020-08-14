ff923d13ec90efa574b57ed9f901cc0299f0cc39
amirak <dashti.amir2752@gmail.com>
2020-08-14 05:13:32 +0430

    change restTemplate url path from vod_auth to stream_auth in authorizeLineForVod

--- src/main/java/com/xtra/core/service/LineService.java
+++ src/main/java/com/xtra/core/service/LineService.java
@@ -46,7 +46,7 @@ public class LineService {
     public LineStatus authorizeLineForVod(String lineToken, String streamToken) {
         RestTemplate restTemplate = new RestTemplate();
         try {
-            return restTemplate.getForObject(mainApiPath + "/lines/vod_auth/" + lineToken + "/" + streamToken, LineStatus.class);
+            return restTemplate.getForObject(mainApiPath + "/lines/stream_auth/" + lineToken + "/" + streamToken, LineStatus.class);
         } catch (HttpClientErrorException exception) {
             System.out.println(exception.getMessage());
             return LineStatus.ERROR;
