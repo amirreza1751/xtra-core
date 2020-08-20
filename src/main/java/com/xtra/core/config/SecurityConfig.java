99a0862244b189f0d30fe0ae1d79a0c7b84938cf
amirak <dashti.amir2752@gmail.com>
2020-08-20 16:36:52 +0430

    add cors disable

---
+++ src/main/java/com/xtra/core/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.xtra.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // Other methods
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**")
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
+
+    }
+}
