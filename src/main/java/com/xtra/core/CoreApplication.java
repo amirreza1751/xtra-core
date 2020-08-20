c0e56f77ec23f8c537a4947cc00753360eeecf96
Alireza Dhb <alireza.dhb@gmail.com>
2020-08-20 18:13:45 +0430

    fix real cors

--- src/main/java/com/xtra/core/CoreApplication.java
+++ src/main/java/com/xtra/core/CoreApplication.java
@@ -7,7 +7,10 @@ import com.xtra.core.repository.ProgressInfoRepository;
 import com.xtra.core.repository.StreamInfoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
 
@@ -28,6 +31,16 @@ public class CoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "PATCH", "POST", "DELETE");
+            }
+        };
     }
 
     @PostConstruct
