ef940cc31d3b8223201df003a7136491dd95d0c6
amirak <dashti.amir2752@gmail.com>
2020-08-30 13:09:20 +0430

    move list method from FileSystemController to FileSystemService

--- src/main/java/com/xtra/core/controller/FileSystemController.java
+++ src/main/java/com/xtra/core/controller/FileSystemController.java
@@ -1,5 +1,6 @@
 package com.xtra.core.controller;
 
+import com.xtra.core.service.FileSystemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,35 +17,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/file")
 public class FileSystemController {
-
+    FileSystemService fileSystemService;
 
     @Autowired
-    public FileSystemController() {
+    public FileSystemController(FileSystemService fileSystemService) {
+        this.fileSystemService = fileSystemService;
     }
 
     @GetMapping("/list_files")
     public List<com.xtra.core.model.File> list(@RequestParam String path) {
-        File directoryPath = new File(path);
-        if (!directoryPath.exists() || !path.startsWith(System.getProperty("user.home"))) {
-            return new ArrayList<>();
-        }
-        File[] filesList = directoryPath.listFiles();
-        ArrayList<com.xtra.core.model.File> result = new ArrayList<>();
-        if (filesList != null) {
-            for (File file : filesList) {
-                if (!file.isHidden()){
-                    result.add(
-                            new com.xtra.core.model.File(
-                                    file.getName(),
-                                    file.getAbsolutePath(),
-                                    (file.isDirectory()) ? file.getTotalSpace() : file.length(),
-                                    file.isDirectory())
-                    );
-                }
-            }
-        }
-
-        return result;
+        return fileSystemService.list(path);
     }
 
 
--- src/main/java/com/xtra/core/service/FileSystemService.java
+++ src/main/java/com/xtra/core/service/FileSystemService.java
@@ -1,7 +1,11 @@
 package com.xtra.core.service;
 
+import com.xtra.core.model.File;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class FileSystemService {
@@ -10,5 +14,27 @@ public class FileSystemService {
     public FileSystemService() {
     }
 
+    public List<File> list(String path) {
+        java.io.File directoryPath = new java.io.File(path);
+        if (!directoryPath.exists() || !path.startsWith(System.getProperty("user.home"))) {
+            return new ArrayList<>();
+        }
+        java.io.File[] filesList = directoryPath.listFiles();
+        ArrayList<com.xtra.core.model.File> result = new ArrayList<>();
+        if (filesList != null) {
+            for (java.io.File file : filesList) {
+                if (!file.isHidden()){
+                    result.add(
+                            new com.xtra.core.model.File(
+                                    file.getName(),
+                                    file.getAbsolutePath(),
+                                    (file.isDirectory()) ? file.getTotalSpace() : file.length(),
+                                    file.isDirectory())
+                    );
+                }
+            }
+        }
 
+        return result;
+    }
 }
