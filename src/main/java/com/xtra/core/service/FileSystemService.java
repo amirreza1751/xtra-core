d4f0cc7df2bde3eb90b73e77d249e0af87844160
amirak <dashti.amir2752@gmail.com>
2020-08-30 13:02:05 +0430

    add FileSystemService

---
+++ src/main/java/com/xtra/core/service/FileSystemService.java
@@ -0,0 +1,14 @@
+package com.xtra.core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileSystemService {
+
+    @Autowired
+    public FileSystemService() {
+    }
+
+
+}
