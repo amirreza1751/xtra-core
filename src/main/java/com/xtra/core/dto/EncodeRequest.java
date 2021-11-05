45f738237f8165fc9bf68434a42eecd5f2564709
Alireza dhb <Alireza.dhb@gmail.com>
2021-11-05 20:37:48 +0330

    add json naming to encode dtos

--- src/main/java/com/xtra/core/dto/EncodeRequest.java
+++ src/main/java/com/xtra/core/dto/EncodeRequest.java
@@ -1,10 +1,13 @@
 package com.xtra.core.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class EncodeRequest {
     private Long videoId;
     private String sourceLocation;
--- src/main/java/com/xtra/core/dto/EncodeResponse.java
+++ src/main/java/com/xtra/core/dto/EncodeResponse.java
@@ -1,11 +1,14 @@
 package com.xtra.core.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.xtra.core.model.EncodeStatus;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class EncodeResponse {
     EncodeStatus encodeStatus;
     List<VideoInfoView> targetVideoInfos;
